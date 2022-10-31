ed770648866690141a7f52e8759d1d7ca2db2baa
HQ08 <HQ08@predator.group>
2022-10-31 16:36:39 +0200

    dev

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -48,11 +48,10 @@ class BotManController extends Controller
             $user = $bot->getUser();
             $chat_id = $user->getId();
 
-            $message_id = $bot->getMessageId();
 
             Log::debug(json_encode( $user->getInfo()));
             Log::debug(json_encode($chat_id));
-            Log::debug(json_encode($message_id));
+            Log::debug(json_encode($bot->getMessages()));
 
 
             foreach ($files as $file) {
